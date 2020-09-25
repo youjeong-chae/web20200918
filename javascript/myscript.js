bfcec8133ca782a9527087105ce5591c29a3b7c9
youjeong-chae <yjchae16@naver.com>
2020-09-25 16:30:48 +0900

    20200925

---
+++ javascript/myscript.js
@@ -0,0 +1,13 @@
+function show() { 
+    console.log("show function");
+}
+
+// 함수는 html 안해줘도 됌
+
+function $myfunc() {
+    console.log("my function");
+}
+
+function $() {
+    console.log("$ function");
+}
